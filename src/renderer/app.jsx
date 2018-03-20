79fc0ec80d13b576818815d964531aec536c9559
Yoojin Ha <hyj0522kl994@outlook.com>
2018-03-20 10:53:11 +0900

    Update app.jsx

--- src/renderer/app.jsx
+++ src/renderer/app.jsx
@@ -15,12 +15,7 @@ import firebase from "firebase/firebase-browser";
 
 //firebase 초기화하기
   var firebaseconFig = {
-    apiKey: "AIzaSyCFk7UUwWQfCmBowx6W2-cZcSj5kkqfVtk",
-    authDomain: "radio-chat-3a94b.firebaseapp.com",
-    databaseURL: "https://radio-chat-3a94b.firebaseio.com",
-    projectId: "radio-chat-3a94b",
-    storageBucket: "radio-chat-3a94b.appspot.com",
-    messagingSenderId: "786475614806"
+xxxx
   };
   firebase.initializeApp(firebaseconFig);
 
